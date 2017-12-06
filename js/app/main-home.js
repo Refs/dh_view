b8f4bf0ca349c6ed48e4ab5a30f8be05db54d081
Refs <lhx1010339808@outlook.com>
2017-12-06 15:13:18 +0800

    .

---
+++ js/app/main-home.js
@@ -0,0 +1,11 @@
+define([
+    'jquery',
+    'echarts',
+    'bmap',
+    'view-theme'
+   
+],drawEcharts);
+
+function drawEcharts($, echarts,BMapExtension) {
+    console.log(BMapExtension);
+}
--- js/common.js
+++ js/common.js
@@ -1,13 +1,24 @@
 //The build will inline common dependencies into this file.
 
 requirejs.config({
-    baseUrl: './js',
-    paths: {
-      'jquery':                   'vendor/jquery',
-      'bootstrap':                'vendor/bootstrap',
-      'echarts' :                 'vendor/echarts'
+  baseUrl: './js',
+  paths: {
+    'jquery': 'vendor/jquery',
+    'bootstrap': 'vendor/bootstrap',
+    'echarts': 'vendor/echarts',
+    'bmap': 'vendor/bmap',
+    'view-theme': 'vendor/view-theme'
+  },
+  shim: {
+    'bootstrap': {　　　　　　　　
+      deps: ['jquery']　　　　　　
     },
-    shim: {
-      'bootstrap':                ['jquery']
+    'bmap': {　　　　　　　　
+      deps: ['echarts'],
+　　　 exports: 'BMapExtension'　　　　　　
+    },
+    'echarts-theme': {　　　　　　　　
+      deps: ['echarts'],
     }
+  }
 });
